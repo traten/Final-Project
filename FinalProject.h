1a9ba602787d6295ef1c25862f840419b5570e15
traten <traten11@gmail.com>
2017-03-21 17:48:41 -0400

    Update FinalProject.h

--- FinalProject.h
+++ FinalProject.h
@@ -9,31 +9,7 @@ class MovieChooser
 {
 public:
   void menu();
-  void userCreateLogIn();
 };
-string userName = " ";
-string password = " ";
-int countUser = 0;
-int countPas = 0;
-//std::string *password; // pointer to password array
-//std::string *userName; // pointer to username array
-//string userName = new (nothrow) string[countUser]; //array to store usernames, may need to change to a linked list
-//string password = new (nothrow) string[countPas]; //array to store passwords, may need to change to a linked list
-std::vector<string> password;
-std::vector<string> userName;
-
-void userCreateLogIn() //usernames linked with passwords
-{
-  std::cout <<"Please type in a username: " << std::endl;
-  std::cin >> userName;
-  countUser++;
-  userName[countUser] = userName; //puts username into an array
-  std::cout << "Please type in a password: " << std::endl;
-  std::cin >> password;
-  countPas++;
-  password[countPas] = password; //puts password into an array
-}
-
 
 int menu()
 {

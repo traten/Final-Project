73ae2e95577e83f8017b64f8f49e7a1173bac1d2
traten <traten11@gmail.com>
2017-03-21 18:21:24 -0400

    Update FinalProject.h

--- FinalProject.h
+++ FinalProject.h
@@ -17,6 +17,8 @@ int menu()
   std::cout<< "1. Take Quiz" << std::endl;
   std::cout << "2. Print Movie List" << std::endl;
   std::cout << "3. Log out" << std::endl;
+  std::cout << "4. Programmers Top Picks" << std::endl;
+  std::cout << "5. Exit System" << std::endl;
   std::cin >> userInput;
   return userInput;
 }

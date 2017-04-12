1c7ea23d892d2f48a3bc280d33e0afd21032066e
k96payne <k96payne@gmail.com>
2017-04-12 16:49:23 -0400

    Add files via upload

---
+++ movieList.h
@@ -0,0 +1,44 @@
+#ifndef movieListTest
+#define movieListTest
+#include <string>
+#include <cstdlib>
+#include <iostream>
+#include <stack>
+#include <fstream>
+using namespace std;
+
+class movieList
+{
+	private:
+		string * movies;
+
+	public:
+		movieList(int size, string fileName);
+		string getMovie(int index);
+};
+
+movieList::movieList(int size, string fileName)
+{
+	this->movies = new string[size];
+
+	std::ifstream movieFile(fileName, fstream::in); //Create file
+
+	/*---Load file into array---*/
+
+	int location = 0;
+
+	string line;
+
+	while (std::getline(movieFile, line))
+	{
+		movies[location] = line;
+		location++;
+	}
+}
+
+string movieList::getMovie(int index)
+{
+	return this->movies[index];
+}
+
+#endif

5c7d052ef793680715a57e302d513b4648138c9d
cseiler16 <cseiler@ufl.edu>
2017-04-13 00:31:22 -0400

    Update quiz.h

--- quiz.h
+++ quiz.h
@@ -70,10 +70,10 @@ void question2() {
     string traitMod = "";
     std::cout << "Question 2" << endl;
     std::cout << "If you could choose any of the following drinks, which would you choose?" << endl;
-    std::cout << "A) A bottle of scotch" << endl;
-    std::cout << "B) Orange juice" << endl;
-    std::cout << "C) The tears of my enemies" << endl;
-    std::cout << "D) Water \n" << endl;
+    std::cout << "1) A bottle of scotch" << endl;
+    std::cout << "2) Orange juice" << endl;
+    std::cout << "3) The tears of my enemies" << endl;
+    std::cout << "4) Water \n" << endl;
     cin >> choice;
     choiceCheck(choice);
     switch(choice){
@@ -101,10 +101,10 @@ void question3() {
     string traitMod = "";
      std::cout << "Question 3" << endl;
     std::cout << "Someone breaks into your house. What is your reaction?" << endl;
-    std::cout << "A) Attempt to fight them" << endl;
-    std::cout << "B) Call the police" << endl;
-    std::cout << "C) Hide" << endl;
-    std::cout << "D) Set up Home Alone-style pranks \n" << endl; 
+    std::cout << "1) Attempt to fight them" << endl;
+    std::cout << "2) Call the police" << endl;
+    std::cout << "3) Hide" << endl;
+    std::cout << "4) Set up Home Alone-style pranks \n" << endl; 
     cin >> choice;
     choiceCheck(choice);
     switch(choice){
@@ -134,10 +134,10 @@ void question4() {
     string traitMod = "";
     std::cout << "Question 4" << endl;
     std::cout << "You are alone in the woods. What do you do?" <<endl;
-    std::cout << "A) Panic" << endl;
-    std::cout << "B) Make a weapon" << endl;
-    std::cout << "C) Start a fire" << endl;
-    std::cout << "D) Pray for help \n" << endl;
+    std::cout << "1) Panic" << endl;
+    std::cout << "2) Make a weapon" << endl;
+    std::cout << "3) Start a fire" << endl;
+    std::cout << "4) Pray for help \n" << endl;
     cin >> choice;
     choiceCheck(choice);
     switch(choice){
@@ -165,10 +165,10 @@ void question5() {
     string traitMod = "";
     std::cout << "Question 5" << endl;
     std::cout << "You are stranded on an island and can only bring one item. What is it?" <<endl;
-    std::cout << "A) A rosary" << endl;
-    std::cout << "B) A knife" << endl;
-    std::cout << "C) A box of matches" << endl;
-    std::cout << "D) A puppy \n" << endl;
+    std::cout << "1) A rosary" << endl;
+    std::cout << "2) A knife" << endl;
+    std::cout << "3) A box of matches" << endl;
+    std::cout << "4) A puppy \n" << endl;
     cin >> choice;
     choiceCheck(choice);
     switch(choice){
@@ -196,10 +196,10 @@ void question6() {
     string traitMod = "";
     std::cout << "Question 6" << endl;
     std::cout << "What is your activity of choice on a Sunday?" << endl;
-    std::cout << "A) Go to church" << endl;
-    std::cout << "B) Stay inside and sleep. The outside world is scary." << endl;
-    std::cout << "C) Go for a hike" << endl;
-    std::cout << "Do a puzzle \n" << endl;
+    std::cout << "1) Go to church" << endl;
+    std::cout << "2) Stay inside and sleep. The outside world is scary." << endl;
+    std::cout << "3) Go for a hike" << endl;
+    std::cout << "4)Do a puzzle \n" << endl;
     cin >> choice;
     choiceCheck(choice);
     switch(choice){
@@ -227,10 +227,10 @@ void question7() {
     string traitMod = "";
     std::cout << "Question 7" << endl;
     std::cout << "You are running late to an exam. While you are running to your testing site, you pass a young kid getting bullied out of his lunch money by some older children. Do you:" << endl;
-    std::cout << "A) Confront the bullies and beat them up" << endl;
-    std::cout << "B) Keep running to your test site. Gotta get that A!" << endl;
-    std::cout << "C) Give the kid a dollar to buy lunch." << endl;
-    std::cout << "D) Call over a teacher \n" << endl;
+    std::cout << "1) Confront the bullies and beat them up" << endl;
+    std::cout << "2) Keep running to your test site. Gotta get that A!" << endl;
+    std::cout << "3) Give the kid a dollar to buy lunch." << endl;
+    std::cout << "4) Call over a teacher \n" << endl;
     cin >> choice;
     choiceCheck(choice);
     switch(choice){
@@ -258,10 +258,10 @@ void question8() {
     string traitMod = "";
     std::cout << "Question 8" << endl;
     std::cout << "You just got an F on your exam (maybe because you stopped to help a bullied kid?). How do you react?" <<endl;
-    std::cout << "A) Eh, it's whatever." << endl;
-    std::cout << "B) Confront the professor about your grade." << endl;
-    std::cout << "C) Wallow in the pain of your failure, eat a pint of ice cream, and sleep" << endl;
-    std::cout << "D) Study much harder for the next one. You can redeem yourself. \n" <<endl;
+    std::cout << "1) Eh, it's whatever." << endl;
+    std::cout << "2) Confront the professor about your grade." << endl;
+    std::cout << "3) Wallow in the pain of your failure, eat a pint of ice cream, and sleep" << endl;
+    std::cout << "4) Study much harder for the next one. You can redeem yourself. \n" <<endl;
     cin >> choice;
     choiceCheck(choice);
     switch(choice){
@@ -289,10 +289,10 @@ void question9() {
     string traitMod = "";
     std::cout << "Question 9" << endl;
     std::cout << "You find an envelope on the street and inside it is $1000. Do you:" <<endl;
-    std::cout << "A) Keep it. Finders Keepers" << endl;
-    std::cout << "B) Donate it" <<endl;
-    std::cout << "C) Try and find the rightful owner" << endl;
-    std::cout << "D) Burn it because the government is oppressive and this is the best way to get back at them. HA! \n" <<endl;
+    std::cout << "1) Keep it. Finders Keepers" << endl;
+    std::cout << "2) Donate it" <<endl;
+    std::cout << "3) Try and find the rightful owner" << endl;
+    std::cout << "4) Burn it because the government is oppressive and this is the best way to get back at them. HA! \n" <<endl;
     cin >> choice;
     choiceCheck(choice);
     switch(choice){
@@ -322,10 +322,10 @@ void question10() {
     string traitMod = "";
     std::cout << "Question 10" << endl;
     std::cout << "What is your favorite color?" <<endl;
-    std::cout << "A) Red" <<endl;
-    std::cout << "B) Blue" <<endl;
-    std::cout << "C) Black" <<endl;
-    std::cout << "D) Yellow \n" <<endl;
+    std::cout << "1) Red" <<endl;
+    std::cout << "2) Blue" <<endl;
+    std::cout << "3) Black" <<endl;
+    std::cout << "4) Yellow \n" <<endl;
     cin >> choice;
     choiceCheck(choice);
     switch(choice){
@@ -353,10 +353,10 @@ void question11() {
     string traitMod = "";
     std::cout << "Question 11" << endl;
     std::cout << "If you could meet one of the following Georges, who would it be?" << endl;
-    std::cout << "A) George Washington" << endl;
-    std::cout << "B) George Foreman" <<endl;
-    std::cout << "C) George Lopez" <<endl;
-    std::cout << "D) Curious George \n" <<endl;
+    std::cout << "1) George Washington" << endl;
+    std::cout << "2) George Foreman" <<endl;
+    std::cout << "3) George Lopez" <<endl;
+    std::cout << "4) Curious George \n" <<endl;
     cin >> choice;
     choiceCheck(choice);
     switch(choice){
@@ -384,10 +384,10 @@ void question12() {
     string traitMod = "";
     std::cout << "Question 12" << endl;
     std::cout << "If you could have any of the following skills, which one would you choose?" <<endl;
-    std::cout << "A) Time travel, but only to the past" << endl;
-    std::cout << "B) Transform, but only into household items" <<endl;
-    std::cout << "C) Instantly read people's minds, but only when they think rude thoughts about you" << endl;
-    std::cout << "D) Tame animals, but only infant raccoons \n" << endl;
+    std::cout << "1) Time travel, but only to the past" << endl;
+    std::cout << "2) Transform, but only into household items" <<endl;
+    std::cout << "3) Instantly read people's minds, but only when they think rude thoughts about you" << endl;
+    std::cout << "4) Tame animals, but only infant raccoons \n" << endl;
     cin >> choice;
     choiceCheck(choice);
     switch(choice){
@@ -417,10 +417,10 @@ void question13() {
     string traitMod = "";
     std::cout << "Question 13" << endl;
     std::cout << "You have a seed. What do you do with it?" << endl;
-    std::cout << "A) Plant it" << endl;
-    std::cout << "B) Eat it. You're hungry now!" << endl;
-    std::cout << "C) Attempt to throw it at a squirrel in the hopes that you will have dinner when the squirrel is knocked out" << endl;
-    std::cout << "D) Talk to it and see if it will tell you its secrets \n" << endl;
+    std::cout << "1) Plant it" << endl;
+    std::cout << "2) Eat it. You're hungry now!" << endl;
+    std::cout << "3) Attempt to throw it at a squirrel in the hopes that you will have dinner when the squirrel is knocked out" << endl;
+    std::cout << "4) Talk to it and see if it will tell you its secrets \n" << endl;
                     
     cin >> choice;
     choiceCheck(choice);
@@ -448,10 +448,10 @@ void question14() {
     string traitMod = "";
     std::cout << "Question 14" << endl;
     std::cout << "You are on vacation in France. What do you do with your time?" << endl;
-    std::cout << "A) Explore the city" << endl;
-    std::cout << "B) Eat lots of food" << endl;
-    std::cout << "C) Stay inside. You don't want to deal with people" << endl;
-    std::cout << "D) Paint the view from your hotel room \n" << endl;
+    std::cout << "1) Explore the city" << endl;
+    std::cout << "2) Eat lots of food" << endl;
+    std::cout << "3) Stay inside. You don't want to deal with people" << endl;
+    std::cout << "4) Paint the view from your hotel room \n" << endl;
                     
     cin >> choice;
     choiceCheck(choice);
